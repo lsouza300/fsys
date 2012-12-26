941ca7e922058826baf43fab46de2cbb405821a0
Leandro Souza <lsouza300@gmail.com>
2012-12-26 10:24:46 -0200

    Primeiro commit.

---
+++ fsysEJB/ejbModule/br/com/manterproduto/RepositorioProduto.java
@@ -0,0 +1,8 @@
+package br.com.manterproduto;
+import br.com.negocio.manterproduto.IRepositorioProduto;
+
+
+public class RepositorioProduto implements IRepositorioProduto {
+
+	public boolean adicionar(){ return true; }
+}
---
+++ fsysEJBClient/ejbModule/br/com/negocio/manterproduto/IRepositorioProduto.java
@@ -0,0 +1,6 @@
+package br.com.negocio.manterproduto;
+
+public interface IRepositorioProduto {
+
+	public boolean adicionar();
+}
